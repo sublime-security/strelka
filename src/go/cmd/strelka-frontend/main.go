01b410d1ee04d92081168f357337bb242caa77d3
Madison Caldwell <madi@sublimesecurity.com>
2023-01-24 12:05:53 -0500

    Add RPC endpoints for compiling YARA, syncing YARA, and checking YARA hash

--- src/go/cmd/strelka-frontend/main.go
+++ src/go/cmd/strelka-frontend/main.go
@@ -10,6 +10,8 @@ import (
 	"io/ioutil"
 	"log"
 	"net"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/go-redis/redis/v8"
@@ -61,11 +63,12 @@ func (s *server) ScanFile(stream strelka.Frontend_ScanFileServer) error {
 	id := uuid.New().String()
 	keyd := fmt.Sprintf("data:%v", id)
 	keye := fmt.Sprintf("event:%v", id)
-	keyy := fmt.Sprintf("yara:%v", id)
+	keyo := fmt.Sprintf("org_id:%s", id)
 
 	var attr *strelka.Attributes
 	var req *strelka.Request
 
+	p := s.coordinator.cli.Pipeline()
 	for {
 		in, err := stream.Recv()
 		if err == io.EOF {
@@ -78,25 +81,19 @@ func (s *server) ScanFile(stream strelka.Frontend_ScanFileServer) error {
 		if attr == nil {
 			attr = in.Attributes
 		}
+
 		if req == nil {
 			req = in.Request
 		}
 
-		hash.Write(in.Data)
-		if len(in.YaraData) > 0 {
-			hash.Write(in.YaraData)
+		if attr.OrgID != "" {
+			p.Set(stream.Context(), keyo, attr.OrgID, time.Until(deadline))
 		}
 
-		p := s.coordinator.cli.Pipeline()
-		p.RPush(stream.Context(), keyd, in.Data)
-		p.ExpireAt(stream.Context(), keyd, deadline)
-
-		// We're using a different pattern for YARA data, because (unlike the file data) it's not chunked.
-		// Additionally, we want to ensure the key stays populated for all exploded sub-documents so that
-		// the YARA can be evaluated against them too. Using the 'rpush/rpop' pattern would be cumbersome
-		// because we'd have to pass the yara data through the scanners and back into the queue for every
-		// sub-document.
-		p.SetNX(stream.Context(), keyy, in.YaraData, time.Until(deadline))
+		if len(in.Data) > 0 {
+			hash.Write(in.Data)
+			p.RPush(stream.Context(), keyd, in.Data)
+		}
 
 		if _, err := p.Exec(stream.Context()); err != nil {
 			return err
@@ -221,6 +218,281 @@ func (s *server) ScanFile(stream strelka.Frontend_ScanFileServer) error {
 		if _, err := (*tx).Exec(stream.Context()); err != nil {
 			return err
 		}
+	}
+
+	return nil
+}
+
+func (s *server) CompileYara(stream strelka.Frontend_CompileYaraServer) error {
+	var req *strelka.Request
+
+	deadline, ok := stream.Context().Deadline()
+	if ok == false {
+		return nil
+	}
+
+	id := uuid.New().String()
+	p := s.coordinator.cli.Pipeline()
+
+	keyYaraCompile := fmt.Sprintf("yara:compile:%s", id)
+	keyYaraCompileDone := fmt.Sprintf("yara:compile:done:%s", id)
+
+	for {
+		in, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		if req == nil {
+			req = in.Request
+		}
+
+		if len(in.Data) > 0 {
+			// Send for compilation
+			p.RPush(stream.Context(), keyYaraCompile, in.Data)
+			p.ExpireAt(stream.Context(), keyYaraCompile, deadline)
+
+			if _, err := p.Exec(stream.Context()); err != nil {
+				return err
+			}
+		}
+	}
+
+	// skip gatekeeper, we're not sending it
+
+	// send task to backend
+	if err := s.coordinator.cli.ZAdd(
+		stream.Context(),
+		"tasks_compile_yara",
+		&redis.Z{
+			Score:  float64(deadline.Unix()),
+			Member: id,
+		},
+	).Err(); err != nil {
+		return err
+	}
+
+	var errMsg string
+
+	for {
+		if err := stream.Context().Err(); err != nil {
+			return fmt.Errorf("context closed: %w", err)
+		}
+
+		res, err := s.coordinator.cli.BLPop(
+			stream.Context(),
+			5*time.Second,
+			keyYaraCompileDone,
+		).Result()
+		if err != nil {
+			if err != redis.Nil {
+				// Delay to prevent fast looping over errors
+				log.Printf("err: %v\n", err)
+				time.Sleep(250 * time.Millisecond)
+			}
+			continue
+		}
+
+		if res[1] == "FIN" {
+			break
+		}
+
+		if strings.HasPrefix(res[1], "ERROR:") {
+			errMsg = strings.Replace(res[1], "ERROR:", "", 1)
+			break
+		}
+	}
+
+	resp := &strelka.CompileYaraResponse{
+		Ok:    errMsg == "",
+		Error: errMsg,
+	}
+
+	if err := stream.Send(resp); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *server) SyncYara(stream strelka.Frontend_SyncYaraServer) error {
+	var orgID string
+	var req *strelka.Request
+
+	deadline, ok := stream.Context().Deadline()
+	if ok == false {
+		return nil
+	}
+
+	id := uuid.New().String()
+	p := s.coordinator.cli.Pipeline()
+
+	var keyYaraHash string
+	keyOrgID := fmt.Sprintf("org_id:%s", id)
+	keyYaraSync := fmt.Sprintf("yara:compile_and_sync:%s", id)
+	keyYaraSyncDone := fmt.Sprintf("yara:compile_and_sync:done:%s", id)
+
+	log.Println("sync yara")
+	for {
+		in, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		if req == nil {
+			req = in.Request
+		}
+
+		if orgID == "" {
+			orgID = in.OrgID
+		}
+
+		keyYaraHash = fmt.Sprintf("yara:hash:%s", orgID)
+		p.Set(stream.Context(), keyOrgID, orgID, time.Until(deadline))
+
+		if len(in.Data) > 0 {
+			for _, inData := range in.Data {
+				outData, err := json.Marshal(*inData)
+				if err != nil {
+					return err
+				}
+
+				// Send for compilation
+				p.RPush(stream.Context(), keyYaraSync, outData)
+				p.ExpireAt(stream.Context(), keyYaraSync, deadline)
+			}
+
+			if _, err := p.Exec(stream.Context()); err != nil {
+				return err
+			}
+		}
+	}
+
+	// skip gatekeeper, we're not sending it
+
+	// send task to backend
+	if err := s.coordinator.cli.ZAdd(
+		stream.Context(),
+		"tasks_compile_and_sync_yara",
+		&redis.Z{
+			Score:  float64(deadline.Unix()),
+			Member: id,
+		},
+	).Err(); err != nil {
+		return err
+	}
+
+	var errMsg string
+
+	for {
+		if err := stream.Context().Err(); err != nil {
+			return fmt.Errorf("context closed: %w", err)
+		}
+
+		res, err := s.coordinator.cli.BLPop(
+			stream.Context(),
+			5*time.Second,
+			keyYaraSyncDone,
+		).Result()
+		if err != nil {
+			if err != redis.Nil {
+				// Delay to prevent fast looping over errors
+				log.Printf("err: %v\n", err)
+				time.Sleep(250 * time.Millisecond)
+			}
+			continue
+		}
+
+		if res[1] == "FIN" {
+			break
+		}
+
+		if strings.HasPrefix(res[1], "ERROR:") {
+			errMsg = strings.Replace(res[1], "ERROR:", "", 1)
+			break
+		}
+	}
+
+	resp := &strelka.SyncYaraResponse{}
+
+	resp.Synced = 0
+	hash, err := s.coordinator.cli.Get(stream.Context(), keyYaraHash).Result()
+	if err != nil {
+		return fmt.Errorf("getting hash: %w", err)
+	}
+
+	synced, err := s.coordinator.cli.Get(stream.Context(), fmt.Sprintf("yara:synced:%s", id)).Result()
+	if err != nil {
+		return fmt.Errorf("getting sync count: %w", err)
+	}
+
+	nSynced, err := strconv.Atoi(synced)
+	if err != nil {
+		// bye bye bye
+		return err
+	}
+
+	resp.Hash = []byte(hash)
+	resp.Error = errMsg
+	resp.Synced = int32(nSynced)
+
+	if err := stream.Send(resp); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *server) ShouldUpdateYara(stream strelka.Frontend_ShouldUpdateYaraServer) error {
+	var keyYaraHash string
+	var orgID string
+	var hash []byte
+
+	for {
+		in, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		if orgID == "" {
+			orgID = in.OrgID
+		}
+
+		if len(hash) == 0 {
+			hash = in.Hash
+		}
+
+		keyYaraHash = fmt.Sprintf("yara:hash:%s", orgID)
+	}
+
+	var currentHash string
+
+	res := s.coordinator.cli.Get(stream.Context(), keyYaraHash)
+	err := res.Err()
+	if err == redis.Nil {
+		// do nothing
+	} else if err != nil {
+		return err
+	}
+
+	currentHash, err = res.Result()
+	if err != nil {
+		return err
+	}
+
+	if err := stream.Send(&strelka.ShouldUpdateYaraResponse{
+		Ok: string(hash) != currentHash,
+	}); err != nil {
+		return err
 	}
 
 	return nil
--- src/python/strelka/scanners/scan_yara.py
+++ src/python/strelka/scanners/scan_yara.py
@@ -1,10 +1,11 @@
+import copy
 import glob
+import hashlib
 import os
 
 import yara
 
-from strelka import strelka
-
+from strelka import strelka, yara_extern
 
 class ScanYara(strelka.Scanner):
     """Scans files with YARA.
@@ -32,13 +33,23 @@ class ScanYara(strelka.Scanner):
         if 'description' not in meta:
             meta.append('description')
 
-        compiled_custom_yara = None
-        if options.get('source'):
+        compiled_custom_yara = []
+        if options.get('compiled_custom_yara'):
             # custom yara was provided - use it to evaluate this file
-            try:
-                compiled_custom_yara = yara.compile(source=options['source'])
-            except (yara.Error, yara.SyntaxError):
-                self.flags.append('compiling_error')
+            compiled_custom_yara.extend(options['compiled_custom_yara'])
+
+        # Support some common external variables
+        externals = copy.copy(yara_extern.EXTERNAL_VARS)
+        externals['filename'] = file.name
+        externals['file_name'] = file.name
+        extension = file.name.split('.')[-1]
+        if extension:
+            externals['extension'] = '.' + file.name.split('.')[-1]
+            externals['filetype'] = extension
+            externals['file_type'] = extension
+        externals['md5'] = hashlib.md5(data).hexdigest()
+        externals['sha1'] = hashlib.sha1(data).hexdigest()
+        externals['sha256'] = hashlib.sha256(data).hexdigest()
 
         try:
             if self.compiled_yara is None and os.path.exists(location):
@@ -46,9 +57,9 @@ class ScanYara(strelka.Scanner):
                     globbed_yara_paths = glob.iglob(f'{location}/**/*.yar*', recursive=True)
                     yara_filepaths = {f'namespace_{i}':entry for (i, entry) in enumerate(globbed_yara_paths)}
                     if yara_filepaths:
-                        self.compiled_yara = yara.compile(filepaths=yara_filepaths)
+                        self.compiled_yara = yara.compile(filepaths=yara_filepaths, externals=externals)
                 else:
-                    self.compiled_yara = yara.compile(filepath=location)
+                    self.compiled_yara = yara.compile(filepath=location, externals=externals)
 
         except (yara.Error, yara.SyntaxError):
             self.flags.append('compiling_error')
@@ -61,15 +72,14 @@ class ScanYara(strelka.Scanner):
             if self.compiled_yara is not None:
                 yara_matches = self.compiled_yara.match(data=data)
 
-            if compiled_custom_yara is not None:
-                custom_yara_matches = compiled_custom_yara.match(data=data)
-                yara_matches.extend(custom_yara_matches)
+            for custom_yara in compiled_custom_yara:
+                yara_matches.extend(custom_yara.match(data=data))
 
             for match in yara_matches:
                 event = { 'name': match.rule, 'tags': [], 'meta': {} }
                 if match.tags:
                     for tag in match.tags:
-                        if not tag in self.event['tags']:
+                        if not tag in event['tags']:
                             event['tags'].append(tag)
 
                 for k, v in match.meta.items():
