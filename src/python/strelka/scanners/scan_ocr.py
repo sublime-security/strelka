843b6605f51eab5910c6a445ac678c89c45d67b5
Ross Wolf <31489089+rw-access@users.noreply.github.com>
2024-12-19 10:11:11 -0700

    Convert PDFs to PNGs before scanning

--- src/python/strelka/scanners/scan_ocr.py
+++ src/python/strelka/scanners/scan_ocr.py
@@ -1,8 +1,8 @@
-import fitz
 import os
 import subprocess
 import tempfile
 
+import fitz
 from strelka import strelka
 
 
@@ -16,15 +16,16 @@ class ScanOcr(strelka.Scanner):
         tmp_directory: Location where tempfile writes temporary files.
             Defaults to '/tmp/'.
     """
-    def scan(self, data, file, options, expire_at):
-        extract_text = options.get('extract_text', False)
-        tmp_directory = options.get('tmp_directory', '/tmp/')
-        pdf_to_png = options.get('pdf_to_png', False)
 
-        if pdf_to_png and 'application/pdf' in file.flavors.get('mime', []):
+    def scan(self, data, file, options, expire_at):
+        extract_text = options.get("extract_text", False)
+        tmp_directory = options.get("tmp_directory", "/tmp/")
+        pdf_to_png = options.get("pdf_to_png", False)
+
+        if pdf_to_png and "application/pdf" in file.flavors.get("mime", []):
             # TODO: Use fitz builtin OCR support which also wraps tesseract
-            doc = fitz.open(stream=data, filetype='pdf')
-            data = doc.get_page_pixmap(0, dpi=150).tobytes('png')
+            doc = fitz.open(stream=data, filetype="pdf")
+            data = doc.get_page_pixmap(0, dpi=150).tobytes("png")
 
         with tempfile.NamedTemporaryFile(dir=tmp_directory) as tmp_data:
             tmp_data.write(data)
@@ -32,24 +33,26 @@ class ScanOcr(strelka.Scanner):
 
             with tempfile.NamedTemporaryFile(dir=tmp_directory) as tmp_tess:
                 tess_return = subprocess.call(
-                    ['tesseract', tmp_data.name, tmp_tess.name],
+                    ["tesseract", tmp_data.name, tmp_tess.name],
                     stdout=subprocess.DEVNULL,
-                    stderr=subprocess.DEVNULL
+                    stderr=subprocess.DEVNULL,
                 )
-                tess_txt_name = f'{tmp_tess.name}.txt'
+                tess_txt_name = f"{tmp_tess.name}.txt"
                 if tess_return == 0:
-                    with open(tess_txt_name, 'rb') as tess_txt:
+                    with open(tess_txt_name, "rb") as tess_txt:
                         ocr_file = tess_txt.read().rstrip()
 
                         # Convert line endings and strip trailing whitespace per line
-                        ocr_file = b'\n'.join([line.rstrip() for line in ocr_file.splitlines()])
+                        ocr_file = b"\n".join(
+                            [line.rstrip() for line in ocr_file.splitlines()]
+                        )
 
                         if ocr_file:
-                            self.event['raw'] = ocr_file
+                            self.event["raw"] = ocr_file
 
                             if extract_text:
                                 extract_file = strelka.File(
-                                    name='text',
+                                    name="text",
                                     source=self.name,
                                 )
 
@@ -63,5 +66,5 @@ class ScanOcr(strelka.Scanner):
                                 self.files.append(extract_file)
 
                 else:
-                    self.flags.append(f'return_code_{tess_return}')
+                    self.flags.append(f"return_code_{tess_return}")
                 os.remove(tess_txt_name)
--- src/python/strelka/scanners/scan_qr.py
+++ src/python/strelka/scanners/scan_qr.py
@@ -7,44 +7,51 @@ from strelka import strelka
 
 # Regex to match URL
 # NOTE: this is overly simplified but will be validated elsewhere
-URL_REGEX = r'^[a-zA-Z]{3,10}:\/\/.*'
+URL_REGEX = r"^[a-zA-Z]{3,10}:\/\/.*"
 
 
 class ScanQr(strelka.Scanner):
     """
     Collects QR code metadata from image files.
     """
+
     def scan(self, data, file, options, expire_at):
+        pdf_to_png = options.get("pdf_to_png", False)
+        if pdf_to_png and "application/pdf" in file.flavors.get("mime", []):
+            # TODO: Use fitz builtin OCR support which also wraps tesseract
+            doc = fitz.open(stream=data, filetype="pdf")
+            data = doc.get_page_pixmap(0, dpi=150).tobytes("png")
+
         try:
             barcodes = decode(Image.open(io.BytesIO(data)))
 
             try:
-                self.event['data'] = barcodes[0].data.decode('utf-8')
+                self.event["data"] = barcodes[0].data.decode("utf-8")
             except:
-                self.flags.append('decode error')
+                self.flags.append("decode error")
                 return
 
             try:
                 # Type: Email
-                if any(qtype in self.event['data'] for qtype in ['MATMSG', 'mailto']):
-                    self.event['type'] = 'email'
+                if any(qtype in self.event["data"] for qtype in ["MATMSG", "mailto"]):
+                    self.event["type"] = "email"
                 # Type: Mobile
-                elif any(qtype in self.event['data'] for qtype in ['tel:', 'sms:']):
-                    self.event['type'] = 'mobile'
+                elif any(qtype in self.event["data"] for qtype in ["tel:", "sms:"]):
+                    self.event["type"] = "mobile"
                 # Type: Geo
-                elif 'geo:' in self.event['data']:
-                    self.event['type'] = 'geo'
+                elif "geo:" in self.event["data"]:
+                    self.event["type"] = "geo"
                 # Type: WIFI
-                elif 'WIFI' in self.event['data']:
-                    self.event['type'] = 'wifi'
+                elif "WIFI" in self.event["data"]:
+                    self.event["type"] = "wifi"
                 # Type: URL
-                elif re.match(URL_REGEX, self.event['data']):
-                    self.event['type'] = 'url'
+                elif re.match(URL_REGEX, self.event["data"]):
+                    self.event["type"] = "url"
                 # Type: No Defined Match
                 else:
-                    self.event['type'] = 'undefined'
+                    self.event["type"] = "undefined"
             except:
-                self.flags.append('parse error')
+                self.flags.append("parse error")
 
         except Exception:
-            self.flags.append('general error')
+            self.flags.append("general error")
